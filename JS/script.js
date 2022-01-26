6439175a41b5b102ab1588907dea03527a920a1d
Carlos Trujillo <augusto21_20@hotmail.com>
2022-01-26 03:11:09 -0500

    Se agrego el elemento hero para darle estilo a la hora de interactuar con el menu hamburguesa

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,17 @@
 // Daremos la interacción del menú hamburguesa
 
+const showNav = (idHamburguer, idContainer, idhero) => {
+    const hamburguer = document.getElementById(idHamburguer),
+        container = document.getElementById(idContainer),
+        hero = document.getElementById(idhero)
+    
+    if (idHamburguer, idContainer, idhero) {
+        hamburguer.addEventListener('click', () => {
+            hamburguer.classList.toggle('container-main-hamburguer--new')
+            container.classList.toggle('main-header__content--new')
+            hero.classList.toggle('main-hero--new')
+        })
+    }
+}
+
+showNav('main-hamburguer', 'main-header__content', 'main-hero');
