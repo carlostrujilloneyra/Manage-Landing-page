78bc4593de1dad6f12df74d0874b8ae070698f4a
Carlos Trujillo <augusto21_20@hotmail.com>
2022-01-27 22:37:43 -0500

    Añadimos el script para las animaciones mediante scroll

---
+++ JS/animaciones.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.main-hero__info', {
+    duration: 1400,
+    origin: 'left',
+    distance: '400px'
+});
+
+sr.reveal('.main-hero__image', {
+    duration: 1400,
+    origin: 'rigth',
+    distance: '400px'
+});
+
+// Main
+
+sr.reveal('.main__first-part', {
+    duration: 1400,
+    origin: 'left',
+    distance: '400px'
+});
+
+sr.reveal('.main__second-part', {
+    duration: 1400,
+    origin: 'rigth',
+    distance: '400px'
+});
+
